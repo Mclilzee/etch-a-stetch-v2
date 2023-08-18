1b67cc684a9582fa73d4e2bf4aaa76bfc56d11f0
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-18 15:23:23 +0200

    Add drawing on mouseover

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
-const drawingBox = document.querySelector<HTMLDivElement>(".drawing-box");
+import frames from "./frames";
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.id = `${i}${j}`;
+const drawingBox = document.querySelector(".drawing-box") as HTMLDivElement;
+let drawInterval: number | undefined;
+let frameIndex = 0;
 
-    drawingBox?.appendChild(cell);
+for (let i = 0; i < 16 * 16; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.id = i.toString();
+
+  drawingBox?.appendChild(cell);
+}
+
+const cells = document.querySelectorAll(".cell") as NodeListOf<HTMLDivElement>;
+
+drawingBox.addEventListener("mouseover", () => {
+  if (drawInterval === undefined) {
+    drawInterval = setInterval(drawFrames, 100);
   }
+})
+
+function drawFrames() {
+  for (let i = 0; i < frames[frameIndex].length; i++) {
+    cells[i].style.backgroundColor = `rgb(${frames[frameIndex][i].r}, ${frames[frameIndex][i].g}, ${frames[frameIndex][i].b})`;
+  }
+
+  frameIndex = (frameIndex + 1) % frames.length;
 }
