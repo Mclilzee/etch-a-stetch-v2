30d5325fc4ffe92664b770bcee745b2b23cd2248
Emad Ali <70924991+Mclilzee@users.noreply.github.com>
2025-11-12 21:55:44 +0100

    Use animation frame instead of interval timer

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import frames from "./frames";
 
 const drawingBox = document.querySelector(".drawing-box") as HTMLDivElement;
 let frameIndex = 0;
-
+let start = 0;
+let currentTime = 0;
 for (let i = 0; i < 16 * 16; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
@@ -12,18 +13,22 @@ for (let i = 0; i < 16 * 16; i++) {
 }
 
 const cells = document.querySelectorAll(".cell") as NodeListOf<HTMLDivElement>;
+drawingBox.addEventListener("mouseover", startAnimation);
 
-drawingBox.addEventListener("mouseover", setDrawInterval);
-
-function setDrawInterval() {
-  setInterval(drawFrames, 100);
-  drawingBox.removeEventListener("mouseover", setDrawInterval);
+function startAnimation() {
+  drawingBox.removeEventListener("mouseover", startAnimation);
+  requestAnimationFrame(drawFrames);
 }
 
 function drawFrames() {
-  for (let i = 0; i < frames[frameIndex].length; i++) {
-    cells[i].style.backgroundColor = `rgb(${frames[frameIndex][i].r}, ${frames[frameIndex][i].g}, ${frames[frameIndex][i].b})`;
+  const elapsed = currentTime - start;
+  if (elapsed > 16) {
+    start = currentTime;
+    for (let i = 0; i < frames[frameIndex].length; i++) {
+      cells[i].style.backgroundColor = `rgb(${frames[frameIndex][i].r}, ${frames[frameIndex][i].g}, ${frames[frameIndex][i].b})`;
+    }
+    frameIndex = (frameIndex + 1) % frames.length;
   }
 
-  frameIndex = (frameIndex + 1) % frames.length;
+  currentTime = requestAnimationFrame(drawFrames);
 }
